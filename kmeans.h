2c468c76224752383d96d48cf27c9ab7f02b6ee5
patrick <patrick.erath@mail.mcgill.ca>
2020-04-17 16:33:23 -0400

    cleanup

--- kmeans.h
+++ kmeans.h
@@ -1,13 +1,7 @@
-// kmeans.h
-// Ethan Brodsky
-// October 2011
-
 extern int* kmeans(
             int  dim,		                     // dimension of data 
-
             double *X,                        // pointer to data
-            int   n,                         // number of elements
-            
+            int   n,                         // number of elements    
             int   k,                         // number of clusters
             double *cluster_centroid         // initial cluster centroids
           // output
--- kmeans_tst.c
+++ kmeans_tst.c
@@ -1,106 +1,92 @@
-#include <stdio.h>
 #define STB_IMAGE_IMPLEMENTATION
+#include <stdio.h>
 #include "stb_image.h"
 #include "kmeans.h"
 #include "kmeans.c"
 #include <math.h>
-int width_;
-int height_;
+#include <time.h>
+
+int WIDTH_GL;
+int HEIGHT_GL;
 double x[615440];
 double *centers;
 int   *cluster_final;
 
 int* kmeans(
-            int  dim,		                     // dimension of data 
-
-            double *X,                        // pointer to data
-            int   n,                         // number of elements
-            
-            int   k,                         // number of clusters
-            double *cluster_centroid 		// initial cluster centroids
+            int  dim,		                  // Data dimensions
+            double *x,                        // Data img_pointer
+            int   n,                          // Number of elements
+            int   k,                          // Number of clusters
+            double *cluster_centroid 		  // Initial cluster centroids
            );
 
-uint8_t* read_in_image() {
-
+uint8_t* read_image(char image_path[]) {
 	int width, height, bpp;
 
-	uint8_t* grayscale_image = stbi_load("image2.jpeg", &width, &height, &bpp, 1);
-
+	// Read in image as grayscale (1 channel)
+	uint8_t* grayscale_image = stbi_load(image_path, &width, &height, &bpp, 1);
 	stbi_image_free(grayscale_image);
 
-	width_ = width;
-	height_ = height;
+	// Set global size variables
+	WIDTH_GL = width;
+	HEIGHT_GL = height;
 
+	// Return image
 	return grayscale_image;
 }
 
 
 int main(int nargs, char **args)
 {
-	uint8_t* pointer = read_in_image();
-	int size = width_ * height_;
+	// Image Path goes heres
+	char IMAGE_PATH[] = "./Images/image.jpeg";
+	char *PATH_POINTER;
+	PATH_POINTER = IMAGE_PATH;
 
+	// Get pointer to 1D-Image array
+	uint8_t* img_pointer = read_image(PATH_POINTER);
+	int size = WIDTH_GL * HEIGHT_GL;
+
+	// Print Image params
+	printf("IMAGE PARAMS: \n");
 	printf("SIZE : ");
 	printf("%d", size);
 	printf("    WIDTH : ");
-	printf("%d", width_);
+	printf("%d", WIDTH_GL);
 	printf("    HEIGHT : ");
-	printf("%d", height_);
+	printf("%d", HEIGHT_GL);
+	printf("\n");
 
-
-	double c[2] = {25.0, 50.0};
+	// Randomly init k-cluster centers
+	int k_clusters = 2;
 	int i;
-	int k = 2;
-
-	centers = calloc(k, sizeof(pointer));
-
-
-	/* populate centroids */
-	for (i = 0; i < k; i++)
-	{
-		centers[i] = (c[i]);
+	srand(time(NULL)); 
+	centers = calloc(k_clusters, sizeof(img_pointer));
+	for(i=0; i<k_clusters; i++){
+		centers[i] = rand() % 255;
 	}
+	printf("\n Randomly Initalized k-clusters");
 
-	// convert data to double array
+	// Convert data to double array
 	double x[size];
 	for(i=0; i< size; i++){
-		//memcpy(r, pointer, sizeof(double));
-		x[i] = (double) pointer[i];
+		x[i] = (double) img_pointer[i];
 	}
 
-	/* run k-means */
-	cluster_final = kmeans(1, x, size, k, centers);
+	// Run K-means on image
+	cluster_final = kmeans(1, x, size, k_clusters, centers);
 
-	for(i=0; i<100; i++){
-		printf("%d", cluster_final[i]);
-	
-	}
-		printf("DONE");
-
-	printf("%d", cluster_final[540000]);
-	printf("%d", cluster_final[545000]);
-	printf("%d", cluster_final[size-1]);
-	// printf("%d", sizeof(cluster_final[size-1]));
-
-
-	
-/**
- * write will write the binary representation of the int, 
- * e.g. 8 bytes forming the 64 bits of an integral value, directly to the file. 
- * There is no conversion into ASCII or UTF8 "characters" as you'd expect when opening the file with a text editor. 
- * The integral value 2 in binary is 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x02,
- * and that's what you "see" in your file. A textual representation would be 0x32, i.e. the ASCII value of digit 2.
- * */
-	FILE *f = fopen("data.txt", "w+");
-	// On success
+	// Write the segmented image file as a binary representation of an int
+	FILE *f = fopen("./Data/data.txt", "w+");
 	if (f) {
         for(i=0; i<size; i++){
 			 fprintf(f, "%d\n", cluster_final[i]);
 		}
         fclose(f);
     }
-
 	free(centers);
+
+	printf("IMAGE SUCESSFULLY SEGMENTED");
 
 	return 0;
 }
