0b34ea481080e0485fd91bd704b55d4400c59eea
patrick <patrick.erath@mail.mcgill.ca>
2020-05-01 10:15:57 -0400

    cleanup

--- kmeans.h
+++ kmeans.h
@@ -1,8 +1,7 @@
 extern int* kmeans(
-            int  dim,		                     // dimension of data 
-            double *X,                        // pointer to data
+            int  dim,		                      // dimension of data 
+            double *X,                        // pointer to array containing image data 
             int   n,                         // number of elements    
             int   k,                         // number of clusters
             double *cluster_centroid         // initial cluster centroids
-          // output
            );
